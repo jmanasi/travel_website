1a25e7fa13685c43b14d7a7fb562153043d836b0
jmanasi <jmanasi954@gmail.com>
2024-05-04 17:40:12 +0530

    Adding the images and adding other sections

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,172 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./footer.css";
+import BeachVideo from "../../Assets/BeachVideo.mp4";
+import { FiSend } from "react-icons/fi";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { FiFacebook } from "react-icons/fi";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FiChevronRight } from "react-icons/fi";
+
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+  return (
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={BeachVideo} loop autoPlay muted type="video/mp4"></video>
+      </div>
+
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small data-aos="fade-up">KEEP IN TOUCH</small>
+            <h2 data-aos="fade-up">Travel With Us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input
+              data-aos="fade-up"
+              type="text"
+              placeholder="Enter Email Address"
+            ></input>
+            <button data-aos="fade-up" className="btn flex" type="submit">
+              SEND <FiSend className="icon" />
+            </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="#" className="logo flex">
+                <MdOutlineTravelExplore className="icon" />
+                Travel.
+              </a>
+            </div>
+
+            <div data-aos="fade-up" className="footerParagraph">
+              Lorem ipsum dolor sit amet. Ut nisi explicabo et quam molestiae
+              aut enim consequatur et voluptas aliquam. Qui alias consectetur
+              quo amet maxime sed ullam pariatur eum cupiditate odio cum
+              praesentium rerum et necessitatibus possimus. Eum minus optio est
+              consequatur consequuntur aut autem porro ut cupiditate architecto
+              et quia placeat.
+            </div>
+
+            <div data-aos="fade-up" className="footerSocials flex">
+              <AiOutlineTwitter className="icon" />
+              <FiFacebook className="icon" />
+              <AiOutlineInstagram className="icon" />
+              <FaTripadvisor className="icon" />
+            </div>
+          </div>
+
+          <div className="footerLinks grid">
+            {/*Group 1 */}
+            <div
+              data-aos="fade-up"
+              data-aos-duration="4000"
+              className="linkGroup"
+            >
+              <span className="groupTitle">OUR AGENCY</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Services
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Insurance
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Agency
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Tourism
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Payment
+              </li>
+            </div>
+
+            {/*Group 2 */}
+            <div
+              data-aos="fade-up"
+              data-aos-duration="4000"
+              className="linkGroup"
+            >
+              <span className="groupTitle">PARTNERS</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Bookings
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Rentcars
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Hostelworld
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Trivago
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                TripAdvisor
+              </li>
+            </div>
+
+            {/*Group 3 */}
+            <div
+              data-aos="fade-up"
+              data-aos-duration="4000"
+              className="linkGroup"
+            >
+              <span className="groupTitle">LAST MINUTE</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                London
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                California
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Indonesia
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Europe
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Oceania
+              </li>
+            </div>
+          </div>
+
+          <div className="footerDiv flex">
+            <small>BEST TRAVEL WEBSITE THEME</small>
+            <small>COPYRIGHTS RESERVED - LOREM IPSUM</small>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,8 +1,168 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./main.css";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { HiOutlineClipboardCheck } from "react-icons/hi";
+import img1 from "../../Assets/img1.jpg";
+import img2 from "../../Assets/img2.jpg";
+import img3 from "../../Assets/img3.jpg";
+import img4 from "../../Assets/img4.jpg";
+import img5 from "../../Assets/img5.jpg";
+import img6 from "../../Assets/img6.jpg";
+import img7 from "../../Assets/img7.jpg";
+import img8 from "../../Assets/img8.jpg";
+import img9 from "../../Assets/img9.jpg";
+
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+const Data = [
+  {
+    id: 1,
+    imgSrc: img1,
+    destTitle: "Bali Island",
+    location: "Indonesia",
+    grade: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Bali is an Indonesian Island and one of the best Holiday destination in the Indonesian archipelago.Bali is known for its volcanic mountain,history,art and culture,food,temples and beautiful sandy beaches",
+  },
+  {
+    id: 2,
+    imgSrc: img2,
+    destTitle: "Sydney",
+    location: "Australia",
+    grade: "CULTURAL RELAX",
+    fees: "$400",
+    description:
+      "Sydney, capital of New South Wales and one of Australia's largest cities, is best known for its harbourfront Sydney Opera House, with a distinctive sail-like design.",
+  },
+  {
+    id: 3,
+    imgSrc: img3,
+    destTitle: "Iceland",
+    location: "Iceland",
+    grade: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "Iceland, a Nordic island nation, is defined by its dramatic landscape with volcanoes, geysers, hot springs and lava fields. Massive glaciers are protected in Vatnajökull and Snæfellsjökull national parks.",
+  },
+  {
+    id: 4,
+    imgSrc: img4,
+    destTitle: "New York city",
+    location: "US",
+    grade: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park.",
+  },
+  {
+    id: 5,
+    imgSrc: img5,
+    destTitle: "Jeju Island",
+    location: "South Korea",
+    grade: "CULTURAL RELAX",
+    fees: "$100",
+    description:
+      "Jeju Island is South Korea's largest island, covering an area of 1,833.2 km², which is 1.83 percent of the total area of the country. Alongside outlying islands, it is part of Jeju Province.",
+  },
+  {
+    id: 6,
+    imgSrc: img6,
+    destTitle: "Ko Phi Phi",
+    location: "Thailand",
+    grade: "CULTURAL RELAX",
+    fees: "$500",
+    description:
+      "The Phi Phi Islands are an island group in Thailand between the large island of Phuket and the Straits of Malacca coast of Thailand. The islands are administratively part of Krabi Province.",
+  },
+  {
+    id: 7,
+    imgSrc: img7,
+    destTitle: "Canada",
+    location: "Canada",
+    grade: "CULTURAL RELAX",
+    fees: "$600",
+    description:
+      "Canada is a country in North America. Its ten provinces and three territories extend from the Atlantic Ocean to the Pacific Ocean and northward into the Arctic Ocean, making it the world's second-largest country by total area, with the world's longest coastline.",
+  },
+  {
+    id: 8,
+    imgSrc: img8,
+    destTitle: "Northern Lights",
+    location: "Norway",
+    grade: "CULTURAL RELAX",
+    fees: "$700",
+    description:
+      "An aurora, also commonly known as the northern lights or southern lights, is a natural light display in Earth's sky, predominantly seen in high-latitude regions.",
+  },
+  {
+    id: 9,
+    imgSrc: img9,
+    destTitle: "Cherry Blossoms",
+    location: "Japan",
+    grade: "CULTURAL RELAX",
+    fees: "$300",
+    description:
+      "The cherry blossom, or sakura, is the flower of trees in Prunus subgenus Cerasus. Sakura usually refers to flowers of ornamental cherry trees, such as cultivars of Prunus serrulata, not trees grown for their fruit.",
+  },
+];
 
 const Main = () => {
-  return <div>Main</div>;
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return (
+    <section className="main container section">
+      <div className="secTitle">
+        <h3 data-aos="fade-right" className="title">
+          Most Viewed Destinations
+        </h3>
+      </div>
+
+      <div className="secContent grid">
+        {Data?.map((item) => {
+          return (
+            <div key={item.id} data-aos="fade-up" className="singleDestination">
+              <div className="imageDiv">
+                <img src={item.imgSrc} alt={item.destTitle} />
+              </div>
+
+              <div className="cardInfo">
+                <h4 className="destTitle">{item.destTitle}</h4>
+                <span className="continent flex">
+                  <HiOutlineLocationMarker className="icon" />
+                  <span className="name">{item.location}</span>
+                </span>
+
+                <div className="fees flex">
+                  <div className="grade">
+                    <span>
+                      {item.grade}
+                      <small>+1</small>
+                    </span>
+                  </div>
+
+                  <div className="price">
+                    <h5>{item.fees}</h5>
+                  </div>
+                </div>
+
+                <div className="desc">
+                  <p>{item.description}</p>
+                </div>
+
+                <button className="btn flex">
+                  DETAILS <HiOutlineClipboardCheck className="icon" />
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Main;
